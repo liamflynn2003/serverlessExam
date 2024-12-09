afeaed3ee92047584a5f2d5d7a04f7459abe2036
liamflynn2003 <20098690@mail.wit.ie>
2024-12-09 10:48:05 +0000

    Function to search for matching Transporters

--- question1/answer.ts
+++ question1/answer.ts
@@ -1,1 +1,57 @@
-console.log('Hello world')
+interface Car {
+    make: string;
+    color: string;
+    registration: string;
+    owner: string;
+}
+
+interface Bicycle {
+    make: string;
+    color: string;
+    owner: string;
+}
+
+type Transporter = Car | Bicycle;
+
+const database: Transporter[] = [
+    {   // Car
+        make: "Toyota Yaris",
+        color: "Red",
+        registration: "231WD1234",
+        owner: "Jane Smith",
+    },
+    {    // Car
+        make: "Suzuki Swift",
+        color: "Blue",
+        registration: "241WD4321",
+        owner: "Paul O Regan",
+    },
+    {   // Car
+        make: "Ford Puma",
+        color: "Blue",
+        registration: "241WD1212",
+        owner: "Eileen Silk",
+    },
+    {   // Bicycle
+        make: "Revel Rascal XO",
+        color: "Blue",
+        owner: "Cindy Tamoka",
+    },
+    {    // Bicycle
+        make: "Yeti SB140 LR",
+        color: "Red",
+        owner: " ",
+    },
+];
+
+function getMatches(
+    criteria: (t: Transporter) => boolean
+) {
+    return database.filter(criteria);
+}
+
+// Get Blue transporters
+console.log(getMatches((t) => t.color === "Blue"))
+
+//Get Red Cars
+console.log(getMatches((t) => t.color === "Red" && "registration" in t))
